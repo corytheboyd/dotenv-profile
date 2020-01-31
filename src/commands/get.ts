11272a40bb1249a895ecc870c8d5fae6d85396f0
Cory Boyd <cory@grooveapp.com>
2020-01-31 15:09:36 -0800

    Adds hard-coded version of get function implementation

--- src/commands/get.ts
+++ src/commands/get.ts
@@ -1,3 +1,11 @@
-export default function(...args: any[]) {
-    console.log(args);
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+export default function(name: string) {
+    const path = resolve(__dirname, '../../.env.test');
+    const result = config({ path });
+    if (result.error) {
+        throw result.error;
+    }
+    console.log(process.env[name]);
 }
