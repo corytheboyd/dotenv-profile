3167fe740a3d51391e0b2a91f61c64352fd7c42c
Cory Boyd <cory@grooveapp.com>
2020-02-03 11:17:44 -0800

    Implements get

--- src/commands/get.ts
+++ src/commands/get.ts
@@ -1,15 +1,25 @@
+import * as Configstore from 'configstore';
 import { config as dotenvConfig } from 'dotenv';
 import { resolve } from 'path';
-import * as Configstore from 'configstore';
+import { homedir } from 'os';
+import { existsSync } from 'fs';
+import * as mkdirp from 'mkdirp';
 
 export default async function(name: string): Promise<void> {
     const config = new Configstore('dotenv-profile');
     const activeProfile = config.get('activeProfile');
+    const directory = resolve(homedir(), '.config', 'dotenv-profile');
+    mkdirp.sync(directory);
+    const fileName = `.env.${activeProfile}`;
+    const filePath = resolve(directory, fileName);
+    if (!existsSync(filePath)) {
+        process.stderr.write(`Profile does not exist: ${activeProfile}\n`);
+        process.exit(1);
+    }
 
-    const path = resolve(__dirname, '../../.env.test');
-    const result = dotenvConfig({ path });
+    const result = dotenvConfig({ path: filePath });
     if (result.error) {
         throw result.error;
     }
-    console.log(process.env[name]);
+    process.stdout.write(process.env[name] || '');
 }
