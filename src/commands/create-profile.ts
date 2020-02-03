3a6d22d6861c25b28781e9672eaf2d3b270f72fb
Cory Boyd <cory@grooveapp.com>
2020-02-03 09:39:40 -0800

    Implements create-profile command

--- src/commands/create-profile.ts
+++ src/commands/create-profile.ts
@@ -1,6 +1,10 @@
 import * as Configstore from 'configstore';
+import { existsSync, writeFileSync } from 'fs';
+import { resolve } from 'path';
+import { homedir } from 'os';
+import * as mkdirp from 'mkdirp';
 
-export default function(profile: string): void {
+export default async function(profile: string): Promise<void> {
     const config = new Configstore('dotenv-profile');
 
     // TODO ensure it exists first
@@ -16,5 +20,14 @@ export default function(profile: string): void {
         config.set('profiles', [profile]);
     }
 
-    console.log(config.all);
+    const directory = resolve(homedir(), '.config', 'dotenv-profile');
+
+    mkdirp.sync(directory);
+
+    const fileName = `.env.${profile}`;
+    const filePath = resolve(directory, fileName);
+
+    if (!existsSync(filePath)) {
+        writeFileSync(filePath, null);
+    }
 }
